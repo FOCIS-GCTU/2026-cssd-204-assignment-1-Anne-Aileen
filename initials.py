27c4e0356da42678d2bafb756f51c25e41b31331
Anne-Aileen <jaileen234@gmail.com>
2026-06-01 05:09:05 +0000

    final initials assignment submission

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints stylized large initials with specific dimensions and layouts.
+# Assignment Number: 1
+#
+# Name: Johnson Oluwadamilola Anne-Aileen
+# STUDENT ID: 2425403131
+# Email: 2425403131@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Johnson Oluwadamilola Anne-Aileen, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    print()
 
+    print("...JOA")
+
+    print()
+
+    print("...JJJJJJJJJJJJJ...0OOOOOOOOO................AAA...............")
+    print(".........JJJJ.....OO........OO..............AA.AA..............")
+    print(".........JJJJ.....OO........OO.............AA...AA.............")
+    print(".........JJJJ.....OO........OO............AA.....AA............")
+    print(".........JJJJ.....OO........OO...........AA.......AA...........")
+    print(".........JJJJ.....OO........OO..........AAAAAAAAAAAAA..........")
+    print(".........JJJJ.....OO........OO.........AA...........AA.........")
+    print("...JJ....JJJJ.....OO........OO........AA.............AA........")
+    print("....JJ...JJJJ.....OO........OO.......AA...............AA.......")
+    print(".....JJJJJJJ..**...OOOOOOOOOO..**...AA.................AA..**..")
 
 
 main()
